19ec534e1fb12d051f92bbefda856567f91609d0
Administrator <admin@example.com>
2017-10-15 14:56:59 +0700

    add dockerfile to build

---
+++ api/main.py
@@ -0,0 +1,21 @@
+import falcon
+import requests
+import json
+import logging
+import os
+
+class MainHandler:
+	"""docstring for MainHandler"""
+	def on_get(self, req, resp):
+		try:
+			data = "Hello Welcome to Docker -> Gunicorn -> Falcon"
+			resp.body = data
+		except Exception, e:
+			print e
+			resp.body = "error"
+ 
+api = falcon.API()
+api.add_route('/', MainHandler())
+
+
+
